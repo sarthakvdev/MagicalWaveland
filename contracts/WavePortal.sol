ecffc0f8b65a2ccc7fe6af74da0e4758d00e443c
Sarthak Verma <sarthakvdev@gmail.com>
2021-08-31 02:06:43 +0530

    kickoff session

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -2,8 +2,16 @@
 pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
-contract WavePortial {
+contract WavePortal {
+    // automatically initialized with 0
+    uint totalWaves;
+
     constructor() {
-        console.log("Naruto Uzumaki");
+        console.log("Naruto Uzumaki, what's up mate?");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        console.log("Someone just waved, woah! Total Waves = %s", totalWaves);
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,17 +1,24 @@
 async function main() {
-    // bug: not able to fund "WavePortal" ???
-    const waveContractFactory = await hre.ethers.getContractFactory("WavePortal")
-    const waveContract = await waveContractFactory.deploy()
+    const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+    const waveContract = await waveContractFactory.deploy();
 
-    // waiting for a transaction to be mined
-    await waveContract.deployed()
+    // waiting for a transaction to be mined, hardhat imitated it
+    await waveContract.deployed();
 
-    console.log("Contract Deployed -- YAY!!! ", waveContract.address)
+    // run the wave function
+    await waveContract.wave();
+    await waveContract.wave();
+    await waveContract.wave();
+    await waveContract.wave();
+    await waveContract.wave();
+
+    console.log("Contract Deployed, YAY!!! ", waveContract.address);
 }
 
+// because it's returning a promise
 main()
     .then(() => process.exit(0))
     .catch((error) => {
-        console.log(error)
-        process.exit(1)
-    })
+        console.log(error);
+        process.exit(1);
+    });
